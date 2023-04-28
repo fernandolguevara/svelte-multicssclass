f296768ca8b674bdf0fe8ed71d0772c5676cedb8
Fernando López Guevara <fernando.lguevara@gmail.com>
2023-04-28 09:25:09 -0300

    chore: test plugin with hrm

--- test/hmr/vite.config.js
+++ test/hmr/vite.config.js
@@ -1,5 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { multicssclass } from '../../src/plugin';
+import { multicssclass } from '../../dist/src/plugin';
 import UnoCSS from 'unocss/vite';
 import { defineConfig } from 'vite';
 import presetUno from '@unocss/preset-uno';
